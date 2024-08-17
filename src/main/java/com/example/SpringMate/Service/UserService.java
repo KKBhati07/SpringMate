2c7bff3db8b8b0c1fb267eb9e5c2dafec5904bf7
Kanishk Bhati <kk.bhatii.007@gmail.com>
2024-08-17 12:40:29 +0530

    added-logging

--- src/main/java/com/example/SpringMate/Service/UserService.java
+++ src/main/java/com/example/SpringMate/Service/UserService.java
@@ -2,6 +2,9 @@ package com.example.SpringMate.Service;
 import com.example.SpringMate.DTO.UserDTO;
 import com.example.SpringMate.Entity.User;
 import com.example.SpringMate.Repositoy.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +12,10 @@ import java.util.HashMap;
 import java.util.List;
 
 @Service
+@Slf4j //will create instance for logger
 public class UserService {
+
+//    private static final Logger log = LoggerFactory.getLogger(UserService.class); //creating instance of logger
     private final UserRepository userRepository;
     @Autowired
     public UserService(UserRepository userRepository){
@@ -26,14 +32,14 @@ public class UserService {
                 res.put("message", "User created successfully");
                 res.put("created", true);
             }else {
+                log.error("User already exists with email :: "+userDetails.getEmail());
                 res.put("message", "User already exists");
                 res.put("created", false);
             }
 
             return res;
         }catch (Exception e){
-            System.out.println(">>>>>"+e.toString());
-            System.out.println(">>>>>   "+e.getMessage());
+            log.error(e.getMessage());
             res.put("message", "Unable to create user");
             res.put("created", false);
             return res;
