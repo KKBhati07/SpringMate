29667c514d6e3577f46aff7a5a96b22fd392e584
Kanishk Bhati <kk.bhatii.007@gmail.com>
2024-08-20 17:40:39 +0530

    added-origin-policy

--- src/main/java/com/example/SpringMate/Constants.java
+++ src/main/java/com/example/SpringMate/Constants.java
@@ -3,6 +3,11 @@ package com.example.SpringMate;
 public class Constants {
     private Constants() {}
 
+    public static class Origin{
+        public static final String DEV = "http://localhost:4200";
+        public static final String PROD = "";
+    }
+
     public static final class UserRole{
         public static final String USER = "USER";
         public static final String ADMIN = "ADMIN";
